2fdb15bc916a194bee207fc9ec75be7db0ecbf40
upkl <2737384+upkl@users.noreply.github.com>
2023-08-13 22:11:14 +0200

    rubocop-suggested changes

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -1,52 +1,48 @@
+# frozen_string_literal: true
+
 ROWS = 6
 COLS = 7
 WIN = 4
 
+# the playground for Connect Four
 class Board
   def initialize
-    @state = (1..ROWS).reduce([]) do |rresult, row|
-      rresult.append((1..COLS).reduce([]) do |result, col|
-                       result.append(" ")
-                       result
-                     end
-                    )
-      rresult
+    @state = (1..ROWS).each_with_object([]) do |_, rresult|
+      rresult.append((1..COLS).each_with_object([]) do |_, result|
+                       result.append(' ')
+                     end)
     end
   end
 
   def to_s
-    string = (0...ROWS).to_a.reverse.reduce("") do |rresult, row|
-      rresult += (0...COLS).reduce("|") do |result, col|
-                       result += @state[row][col]
-                       result
-      end                   
-      rresult + "|\n"
+    string = (0...ROWS).to_a.reverse.reduce('') do |rresult, row|
+      rresult += (0...COLS).reduce('|') do |result, col|
+        result += @state[row][col]
+        result
+      end
+      "#{rresult}|\n"
     end
-    string + "+" + "-" * COLS + "+"
+    "#{string}+#{'-' * COLS}+"
   end
 
-  def put col, symbol
+  def put(col, symbol)
     col -= 1
-    column_pieces = (0...ROWS).map { |row| @state[row][col]}
-    row = column_pieces.find_index(" ")
-    if row
-      @state[row][col] = symbol
-    end
+    column_pieces = (0...ROWS).map { |row| @state[row][col] }
+    row = column_pieces.find_index(' ')
+    @state[row][col] = symbol if row
   end
 
   def winner
     win_sequences.each do |sequence|
       get_pieces = sequence.map { |row, col| @state[row][col] }
       get_pieces = get_pieces.sort.uniq
-      if get_pieces.length == 1 and get_pieces[0] != " "
-        return get_pieces[0]
-      end
+      return get_pieces[0] if get_pieces.length == 1 && get_pieces[0] != ' '
     end
-
     nil
   end
 
   private
+
   def win_sequences
     result = []
 
@@ -70,19 +66,13 @@ class Board
       end
     end
 
-    # sw-ne sequences
+    # sw-ne and se-nw sequences
     (0...ROWS - WIN + 1).each do |row|
       (0...COLS - WIN + 1).each do |col|
         sequence = (0...WIN).map do |i|
           [row + i, col + i]
         end
         result << sequence
-      end
-    end
-
-    # se-nw sequences
-    (0...ROWS - WIN + 1).each do |row|
-      (0...COLS - WIN + 1).each do |col|
         sequence = (0...WIN).map do |i|
           [row + WIN - 1 - i, col + i]
         end
@@ -94,48 +84,54 @@ class Board
   end
 end
 
+# Player class
 class Player
   attr_reader :symbol
-  def initialize symbol
+
+  def initialize(symbol)
     @symbol = symbol
   end
 
   def read_move
-    begin
+    answer = ''
+    loop do
       puts "Enter move (1..#{COLS})"
       answer = gets.chomp.to_i
-    end until 1 <= answer && answer <= COLS
+      break if answer.between?(1, COLS)
+    end
     answer
   end
 end
 
+# class for the game logic
 class Game
   def initialize
-    @p1 = Player.new "X"
-    @p2 = Player.new "O"
+    @p1 = Player.new 'X'
+    @p2 = Player.new 'O'
     @board = Board.new
   end
 
-  def move player
-    begin
+  def move(player)
+    loop do
       col = player.read_move
-      ok = @board.put col, player.symbol
-    end until ok
+      break if @board.put(col, player.symbol)
+    end
   end
 
   def run
     winner = nil
     loop do
       puts @board
-      puts "Player 1"
+      puts 'Player 1'
       move @p1
       winner = @board.winner
-      break if !winner.nil?
+      break unless winner.nil?
+
       puts @board
-      puts "Player 2"
+      puts 'Player 2'
       move @p2
       winner = @board.winner
-      break if !winner.nil?
+      break unless winner.nil?
     end
     puts "\"#{winner}\", you win."
   end
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -1,86 +1,85 @@
-require './lib/connect_four.rb'
+# frozen_string_literal: true
+
+require './lib/connect_four'
 
 describe Board do
-  describe "#to_s" do
+  describe '#to_s' do
     subject(:board) { described_class.new }
-    it "returns the correct string" do
-      pieces_line = "|" + " " * COLS + "|"
-      bottom_line = "+" + "-" * COLS + "+"
-      expected = (pieces_line + "\n") * ROWS + bottom_line
+    it 'returns the correct string' do
+      pieces_line = "|#{' ' * COLS}|"
+      bottom_line = "+#{'-' * COLS}+"
+      expected = ("#{pieces_line}\n" * ROWS) + bottom_line
       output = board.to_s
       expect(output).to eq(expected)
     end
   end
 
-  describe "#put" do
+  describe '#put' do
     subject(:board) { described_class.new }
-    it "accepts a single piece" do
-      pieces_line = "|" + " " * COLS + "|"
-      pieces_line_2 = "|A" + " " * (COLS - 1) + "|"
-      bottom_line = "+" + "-" * COLS + "+"
-      expected = (pieces_line + "\n") * (ROWS - 1) + pieces_line_2 \
-               + "\n" + bottom_line
-      expect(board.put 1, "A").to eq "A"
+    it 'accepts a single piece' do
+      pieces_line = "|#{' ' * COLS}|"
+      pieces_line2 = "|A#{' ' * (COLS - 1)}|"
+      bottom_line = "+#{'-' * COLS}+"
+      expected = ("#{pieces_line}\n" * (ROWS - 1)) + "#{pieces_line2}\n#{bottom_line}"
+      expect(board.put(1, 'A')).to eq('A')
       output = board.to_s
       expect(output).to eq(expected)
     end
-    it "accepts two pieces" do
-      pieces_line = "|" + " " * COLS + "|"
-      pieces_line_2 = "|" + " " * (COLS - 1) + "B|"
-      bottom_line = "+" + "-" * COLS + "+"
-      expected = (pieces_line + "\n") * (ROWS - 2) \
-               + (pieces_line_2 + "\n") * 2 \
-               + bottom_line
-      expect(board.put COLS, "B").to eq "B"
-      expect(board.put COLS, "B").to eq "B"
+    it 'accepts two pieces' do
+      pieces_line = "|#{' ' * COLS}|"
+      pieces_line2 = "|#{' ' * (COLS - 1)}B|"
+      bottom_line = "+#{'-' * COLS}+"
+      expected = ("#{pieces_line}\n" * (ROWS - 2)) + ("#{pieces_line2}\n" * 2) + bottom_line
+      expect(board.put(COLS, 'B')).to eq('B')
+      expect(board.put(COLS, 'B')).to eq('B')
       output = board.to_s
       expect(output).to eq(expected)
     end
-    it "refuses to accept more pieces than rows" do
-      (1..ROWS).each { expect(board.put 2, "Z").to eq "Z"}
-      expect(board.put 2, "Z").to be_nil
+    it 'refuses to accept more pieces than rows' do
+      (1..ROWS).each { expect(board.put(2, 'Z')).to eq 'Z' }
+      expect(board.put(2, 'Z')).to be_nil
     end
-    it "orders pieces correctly" do
-      sequence = ["A", "U", "O", "H", "3", "?"]
-      expected_lines = sequence.reverse.map { |piece| "|" + piece + " " * (COLS - 1) + "|" }
-      bottom_line = "+" + "-" * COLS + "+"
-      expected = expected_lines.join("\n") + "\n" + bottom_line
-      sequence.each { |piece| expect(board.put 1, piece).to eq piece }
+    it 'orders pieces correctly' do
+      sequence = ['A', 'U', 'O', 'H', '3', '?']
+      expected_lines = sequence.reverse.map { |piece| "|#{piece}#{' ' * (COLS - 1)}|" }
+      bottom_line = "+#{'-' * COLS}+"
+      expected = expected_lines.join("\n") + "\n#{bottom_line}"
+      sequence.each { |piece| expect(board.put(1, piece)).to eq(piece) }
       output = board.to_s
       expect(output).to eq(expected)
     end
   end
 
-  describe "#winner" do
+  describe '#winner' do
     subject(:board) { described_class.new }
-    it "returns nil on an empty board" do
+    it 'returns nil on an empty board' do
       expect(board.winner).to be_nil
     end
-    it "finds a winner in column 1" do
-      (1..WIN).each { board.put 1, "/"}
-      expect(board.winner).to eq ("/")
+    it 'finds a winner in column 1' do
+      (1..WIN).each { board.put(1, '/') }
+      expect(board.winner).to eq('/')
     end
-    it "does not accept one piece less" do
-      (1..WIN - 1).each { board.put 1, "/"}
+    it 'does not accept one piece less' do
+      (1..WIN - 1).each { board.put(1, '/') }
       expect(board.winner).to be_nil
     end
-    it "does not accept mixed columns" do
-      (1..WIN).each { |n| board.put 1, n.to_s }
+    it 'does not accept mixed columns' do
+      (1..WIN).each { |n| board.put(1, n.to_s) }
       expect(board.winner).to be_nil
     end
-  
-    it "accepts a horizontal sequence" do
-      (1..WIN - 2).each do |row|
-        (1..WIN - 1).each { |col| board.put col, "A" }
-        board.put WIN, "C"
+
+    it 'accepts a horizontal sequence' do
+      (WIN - 2).times do
+        (1..WIN - 1).each { |col| board.put(col, 'A') }
+        board.put(WIN, 'C')
       end
-      (1..WIN).each { |col| board.put col, "B"}
-      expect(board.winner).to eq ("B")
+      (1..WIN).each { |col| board.put(col, 'B') }
+      expect(board.winner).to eq('B')
     end
 
-    it "accepts a diagonal sequence" do
+    it 'accepts a diagonal sequence' do
       (1..WIN).each do |cols|
-        (1..cols).each { |col| board.put col, cols.to_s}
+        (1..cols).each { |col| board.put col, cols.to_s }
       end
       expect(board.winner).to eq(WIN.to_s)
     end
@@ -88,27 +87,22 @@ describe Board do
 end
 
 describe Player do
-  describe "#initialize" do
-    subject(:player) { described_class.new "*" }
-    it "initializes the symbol correctly" do
-      expect(player.symbol).to eq("*")
+  describe '#initialize' do
+    subject(:player) { described_class.new '*' }
+    it 'initializes the symbol correctly' do
+      expect(player.symbol).to eq('*')
     end
   end
 
-  describe "#read_move" do
-    subject(:player) { described_class.new "?" }
-    it "returns a correct value" do
+  describe '#read_move' do
+    subject(:player) { described_class.new '?' }
+    it 'returns a correct value' do
       allow(player).to receive(:gets).and_return("3\n")
       expect(player.read_move).to eq(3)
     end
-    it "rejects invalid input and returns the first correct input" do
+    it 'rejects invalid input and returns the first correct input' do
       allow(player).to receive(:gets).and_return("-1\n", "#{COLS + 1}\n", "#{COLS}\n", "1\n")
       expect(player.read_move).to eq(COLS)
     end
-  end
-end
-
-describe Game do
-  describe "#move" do
   end
 end
